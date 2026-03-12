dcc64217b8110061c8035c26d54c67eb0d36e0a7
AryanP27 <ap391@uakron.edu>
2026-03-12 17:53:00 -0400

    Add function for Bollinger Bands

---
+++ indicators/bollinger.py
@@ -0,0 +1,25 @@
+""" 
+This file contains the Bollinger Bands indicator. 
+
+
+
+
+
+"""
+
+import pandas as pd
+
+def bollinger_bands(prices, window=20):
+    variance = prices.rolling(window).var()
+    sma = prices.rolling(window).mean()
+    std = variance ** 0.5
+    lower_band = sma - (2 * std)
+    upper_band = sma + (2 * std)
+    return sma, lower_band, upper_band
+
+
+prices = pd.Series(range(1, 101))  # Place holder price data. Will be removed soon. 
+sma, lower_band, upper_band = bollinger_bands(prices)
+print("SMA:\n", sma.tail())
+print("Lower Band:\n", lower_band.tail())
+print("Upper Band:\n", upper_band.tail())
