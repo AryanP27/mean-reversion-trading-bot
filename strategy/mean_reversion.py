99bf8e3613eac4f55ad1136648dcac01e67563d7
AryanP27 <ap391@uakron.edu>
2026-03-13 15:18:10 -0400

    Create MeanReversionStrategy Class

--- strategy/mean_reversion.py
+++ strategy/mean_reversion.py
@@ -9,19 +9,20 @@ from ..indicators.bollinger import bollinger_bands
 
 import pandas as pd
 
-def bollinger_signal(price, sma, upper, lower):
 
-    prices = pd.Series(range(1, 101))  # Place holder price data. Will be removed soon. 
-    sma, lower_band, upper_band = bollinger_bands(prices)
-    latest_sma = sma.iloc[-1]
-    latest_lower = lower_band.iloc[-1]
-    latest_upper = upper_band.iloc[-1]
-    latest_price = prices.iloc[-1]
 
-    if price < lower:
-        return "BUY"
-    elif price > upper:
-        return "SELL"
-    else:
-        return "HOLD"
+    
+class MeanReversionStrategy:
+    def __init__(self):
+        self.position = None  # Can be "LONG", "SHORT", or None
+        self.entry_price = None
+
+    def bollinger_signal(price, sma, lower, upper):     
+        if price < lower:
+            return "BUY"
+        elif price > upper:
+            return "SELL"
+        else:
+            return "HOLD"
+
     
